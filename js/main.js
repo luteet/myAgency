987efc4e6b87d81099b5387ee1a67a8ae4d0d833
Roman Lukjanets <64837849+luteet@users.noreply.github.com>
2020-06-29 17:50:27 +0300

    v0.3.4

--- js/main.js
+++ js/main.js
@@ -3,25 +3,19 @@ $(function(){
     let btn_for_slick_left = '<button class="slider__arrows slider__arrows--left"><svg class="slider__icon" version="1.1" xmlns="http://www.w3.org/2000/svg" width="45" height="45" viewBox="0 0 384 448"><title></title><g id="icomoon-ignore"></g><path d="M320 240v-32c0-8.75-7.25-16-16-16h-125.5l47.25-47.25c3-3 4.75-7 4.75-11.25s-1.75-8.25-4.75-11.25l-22.75-22.75c-3-3-7-4.5-11.25-4.5s-8.25 1.5-11.25 4.5l-113.25 113.25c-3 3-4.5 7-4.5 11.25s1.5 8.25 4.5 11.25l113.25 113.25c3 3 7 4.5 11.25 4.5s8.25-1.5 11.25-4.5l22.75-22.75c3-3 4.5-7 4.5-11.25s-1.5-8.25-4.5-11.25l-47.25-47.25h125.5c8.75 0 16-7.25 16-16zM384 224c0 106-86 192-192 192s-192-86-192-192 86-192 192-192 192 86 192 192z"></path></svg></button',
         btn_for_slick_right = '<button class="slider__arrows slider__arrows--right"><svg class="slider__icon" version="1.1" xmlns="http://www.w3.org/2000/svg" width="45" height="45" viewBox="0 0 384 448"><title></title><g id="icomoon-ignore"></g><path d="M321.25 224c0-4.25-1.5-8.25-4.5-11.25l-113.25-113.25c-3-3-7-4.5-11.25-4.5s-8.25 1.5-11.25 4.5l-22.75 22.75c-3 3-4.5 7-4.5 11.25s1.5 8.25 4.5 11.25l47.25 47.25h-125.5c-8.75 0-16 7.25-16 16v32c0 8.75 7.25 16 16 16h125.5l-47.25 47.25c-3 3-4.75 7-4.75 11.25s1.75 8.25 4.75 11.25l22.75 22.75c3 3 7 4.5 11.25 4.5s8.25-1.5 11.25-4.5l113.25-113.25c3-3 4.5-7 4.5-11.25zM384 224c0 106-86 192-192 192s-192-86-192-192 86-192 192-192 192 86 192 192z"></path></svg></button>';
 
-    $('.team__slider').slick({
-        slidesToShow: 3,
-        prevArrow: btn_for_slick_left,
-        nextArrow: btn_for_slick_right,
-        responsive: [
-            {
-              breakpoint: 900,
-              settings: {
-                slidesToShow: 2,
-              }
-            },
-            {
-                breakpoint: 600,
-                settings: {
-                  slidesToShow: 1,
-                }
-              },
-        ]
-    });
+        /* $('.team__list').slick({
+            slidesToShow: 3,
+            prevArrow: btn_for_slick_left,
+            nextArrow: btn_for_slick_right,
+            responsive: [
+                {
+                    breakpoint: 768,
+                    settings: {
+                      slidesToShow: 2,
+                    }
+                  },
+            ]
+        }); */
 
     $('.reviews__slider').slick({
         prevArrow: btn_for_slick_left,
@@ -57,8 +51,6 @@ $(function(){
 
     ThisIsWebP().then(function() {
         $.each($('.image-box'), function (index, val) {
-            
-            console.log(0);
         });
     }
     , function() {
@@ -84,6 +76,7 @@ $(function(){
                 $(phone_btn).appendTo($(menu_list)).css('display', 'block');
                 $(menu).css('display', 'none');
                 $(burger).removeClass('active');
+                
             }
             else if($(window).width() > 768 && phone_btn_check == true){
                 phone_btn_check = false;
@@ -222,6 +215,73 @@ $(function(){
 
     headerHider({
         elemName: $('.header__top'),
-    });    
+    });
+
+    function formCustomer(formSetting) {
+
+        let placeholder,
+            elemLength = 0,
+            i = 1;
+
+
+        if(formSetting.elemName == undefined) {
+            return false;
+        }
+
+        elemLength = formSetting.elemName;
+        
+
+
+        if(formSetting.keySwitch == true) {
+            $(formSetting.elemName).on('keydown', function(e){
+                let keyKod = e.which,
+                    nextElem = $(this).next(formSetting.elemName);
+                    prevElem = $(this).prev(formSetting.elemName);
+                if(keyKod == 40) {
+                    $(nextElem).focus();  
+                }
+                if(keyKod == 38) {
+                    
+                    $(prevElem).focus();  
+                }
+
+            });
+        }
+
+        if(formSetting.tabindex == true) {
+            $.each($(formSetting.elemName), function () {
+                $(this).attr('tabindex') = i;
+                i++;
+            });
+        }
+        
+        if(formSetting.customPlace == true) {
+
+            $.each($(formSetting.elemName), function () {
+                if(!$(this).data('placeholder')) {
+                    return false;
+                }
+                placeholder = $(this).data('placeholder');
+                $(this).attr('placeholder', placeholder);
+            });
+        
+            $(formSetting.elemName).focus(function () {
+                $(this).attr('placeholder', '');
+            });
+        
+            $(formSetting.elemName).blur(function () {
+                placeholder = $(this).data('placeholder');
+                $(this).attr('placeholder', placeholder);
+            });
+
+        }    
+
+}
+
+    formCustomer({
+        elemName: '.form-elem',
+        keySwitch: true,
+        customPlace: true
+    });
 
 });
